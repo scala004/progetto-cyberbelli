0bc6dda4c4ab5431274c138175cb5b6ecc7ab94a
ripslinger21 <64437730+ripslinger21@users.noreply.github.com>
2022-11-25 17:23:29 +0100

    php

--- amministrazione/inserisci_scuola.php
+++ amministrazione/inserisci_scuola.php
@@ -1,5 +1,5 @@
 <?php  
-include("../condivise/config.php"); 
+include("../condivise/config.php");
 $id= $_POST['id_scuola'];
 $nome= $_POST['nome_scuola'];
 $email= $_POST['email'];
@@ -12,13 +12,50 @@ $citta= $_POST['citta'];
 $provincia= $_POST['provincia'];
 $regione = $_POST['regione'];
 
-//CAPIRE COME GESTIRE IL LOGO (INDIRIZZO DELLA CARTELLA CONTENENTE IL FILE)
 
-if (empty($desc)){
-	$query = "insert into scuola(id_scuola,nome_scuola,telefono,email,logo_scuola,sito,indirizzo,cap,citta,provincia,regione) values ('$id','$nome','$tel','$email','logo','$sito','$indirizzo','$cap','$citta','$provincia','$regione')";
-}else{
-	$query = "insert into scuola values('$id','$nome','$tel','$email','logo','$desc','$sito','$indirizzo','$cap','$citta','$provincia','$regione')";   
-}
+	$fileExistsFlag = 0;
+	$fileName = $_FILES['Filename']['name'];
+	/* 
+	*	Checking whether the file already exists in the destination folder 
+	*/
+	$query = "SELECT logo_scuola FROM scuola WHERE logo_scuola='$fileName'";
+	$result = $connect->query($query) or die("Error : ".mysqli_error($connect));
+	while($row = mysqli_fetch_array($result)) {
+		if($row['filename'] == $fileName) {
+			$fileExistsFlag = 1;
+		}		
+	}
+	/*
+	* 	If file is not present in the destination folder
+	*/
+	if($fileExistsFlag == 0) { 
+		$target = "../scuole/img/";		
+		$fileTarget = $target.$fileName;	
+		$tempFileName = $_FILES["Filename"]["tmp_name"];
+		$result = move_uploaded_file($tempFileName,$fileTarget);
+		/*
+		*	If file was successfully uploaded in the destination folder
+		*/
+		if($result) { 
+			echo "Your file <html><b><i>".$fileName."</i></b></html> has been successfully uploaded";	
+			if (empty($desc)){
+				$query = "insert into scuola(id_scuola,nome_scuola,telefono,email,logo_scuola,sito,indirizzo,cap,citta,provincia,regione) values ('$id','$nome','$tel','$email','$fileTarget','$sito','$indirizzo','$cap','$citta','$provincia','$regione')";
+			}else{
+				$query = "insert into scuola values('$id','$nome','$tel','$email','$fileTarget','$desc','$sito','$indirizzo','$cap','$citta','$provincia','$regione')";   
+			}
+			$connect->query($query) or die("Error : ".mysqli_error($connect));			
+		}
+		else {			
+			echo "Sorry !!! There was an error in uploading your file";			
+		}
+	}
+	/*
+	* 	If file is already present in the destination folder
+	*/
+	else {
+		echo "File <html><b><i>".$fileName."</i></b></html> already exists in your folder. Please rename the file and try again.";
+	}
+
 
 $result = mysqli_query($connect,$query) or die (mysqli_error($connect));
 
