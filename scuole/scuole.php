326dcbabbe76dd1990500a99e32666be65a11562
ripslinger21 <64437730+ripslinger21@users.noreply.github.com>
2022-12-02 18:46:05 +0100

    favicon

--- scuole/scuole.php
+++ scuole/scuole.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="../condivise/favicon/icona scuole.png" type="image/png" />
     <title>Scuole aderenti</title>
     <link rel="stylesheet" href="../condivise/stylesheet.css" />
   </head>
